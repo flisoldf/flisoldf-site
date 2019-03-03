de6bb02b67f183d15063a9e7a00e9281cd638ee8
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2019-03-03 12:44:03 -0300

    Revisão da interface

--- 2019/assets/js/demo.js
+++ 2019/assets/js/demo.js
@@ -1,27 +1,27 @@
 var big_image;
 
 demo = {
-    initContactUsMap: function(){
+    // initContactUsMap: function(){
 
-        var myLatlng = new google.maps.LatLng(-15.8430268, -48.1003822);
-        var mapOptions = {
-          zoom: 16,
-          center: myLatlng,
-          //styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
-          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        }
+    //     var myLatlng = new google.maps.LatLng(-15.8430268, -48.1003822);
+    //     var mapOptions = {
+    //       zoom: 16,
+    //       center: myLatlng,
+    //       //styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
+    //       scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+    //     }
 
-        var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+    //     var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
 
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"FLISOL DF 2019"
-        });
+    //     var marker = new google.maps.Marker({
+    //         position: myLatlng,
+    //         title:"FLISOL DF 2019"
+    //     });
 
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
+    //     // To add the marker to the map, call setMap();
+    //     marker.setMap(map);
 
-    },
+    // },
 
     initContactUsMap2: function(){
 
@@ -96,11 +96,6 @@ demo = {
     	}
     }
 }
-
-$(document).ready(function(){
-
-    demo.verticalDots();
-});
 
 $('a[data-scroll="true"]').click(function(e){
     var scroll_target = $(this).data('id');
